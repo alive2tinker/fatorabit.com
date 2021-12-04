2252a8248dc8e7d1693ac9cfdb9297c71c94e089
Abdulmalik Salman Alsufayran <abdulmalik.alsufayran@sa.zain.com>
2021-12-04 13:35:54 +0300

    added uuid

--- database/migrations/2021_12_04_133435_remove_uuid_from_invoices.php
+++ database/migrations/2021_12_04_133435_remove_uuid_from_invoices.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddUuidToInvoices extends Migration
+class RemoveUuidFromInvoices extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,8 @@ class AddUuidToInvoices extends Migration
     public function up()
     {
         Schema::table('invoices', function (Blueprint $table) {
-            $table->uuid('uuid')->unique();
+            $table->dropColumn('uuid');
+            $table->dropUnique('uuid');
         });
     }
 
@@ -26,7 +27,7 @@ class AddUuidToInvoices extends Migration
     public function down()
     {
         Schema::table('invoices', function (Blueprint $table) {
-            $table->dropColumn('uuid');
+            $table->uuid('uuid')->unique();
         });
     }
 }
